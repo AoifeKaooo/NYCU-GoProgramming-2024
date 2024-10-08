df376256123106a3c179ed60252cab913d482fed
Aoife <amy0986288609@gmail.com>
2024-10-08 10:56:38 +0800

    lab2

--- lab1/lab1.go
+++ lab1/lab1.go
@@ -18,4 +18,20 @@ func main() {
 	fmt.Println("Divide:", Div(a, b))
 }
 
-// TODO: Create `Add`, `Sub`, `Mul`, `Div` function here
+
+
+func Add(a int64, b int64) int64{
+	return a+b;
+}
+
+func Sub(a int64, b int64) int64{
+	return a-b;
+}
+
+func Mul(a int64, b int64) int64{
+	return a*b;
+}
+
+func Div(a int64, b int64) int64{
+	return a/b;
+}
--- lab2/lab2.go
+++ lab2/lab2.go
@@ -13,5 +13,19 @@ func main() {
 }
 
 func Sum(n int64) string {
-	// TODO: Finish this function
+	var sum int64 = 0
+	var expression string
+
+	for i := int64(1); i <= n; i++ {
+		if i%7 != 0 { // 只累加不是 7 的倍數的數字
+			if sum > 0 {
+				expression += "+" // 如果不是第一個數字，就在前面加上 "+"
+			}
+			expression += fmt.Sprintf("%d", i) // 將數字加入運算式
+			sum += i
+		}
+	}
+
+	expression += fmt.Sprintf("=%d", sum) // 最後加上等號和總和
+	return expression
 }
