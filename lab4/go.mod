1c54158644a2202cbe05ac50fe587cc3de2a33b3
Aoifekaooo <amy0986288609@gmail.com>
2024-10-22 12:34:37 +0800

    lab4

---
+++ lab4/go.mod
@@ -0,0 +1,30 @@
+module lab4
+
+go 1.23.2
+
+require (
+	github.com/PuerkitoBio/goquery v1.10.0
+	github.com/stretchr/testify v1.9.0
+)
+
+require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/exp/typeparams v0.0.0-20221212164502-fae10dda9338 // indirect
+	golang.org/x/mod v0.20.0 // indirect
+	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/telemetry v0.0.0-20240829154258-f29ab539cc98 // indirect
+	golang.org/x/text v0.18.0 // indirect
+	golang.org/x/tools v0.22.1-0.20240829175637-39126e24d653 // indirect
+	golang.org/x/tools/gopls v0.16.2 // indirect
+	golang.org/x/vuln v1.0.4 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	honnef.co/go/tools v0.4.7 // indirect
+	mvdan.cc/gofumpt v0.6.0 // indirect
+	mvdan.cc/xurls/v2 v2.5.0 // indirect
+)
---
+++ lab4/lab4.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+type PageData struct {
+	Expression string
+	Result     string
+	Error      string
+}
+
+// Calculator function to handle calculations
+func Calculator(w http.ResponseWriter, r *http.Request) {
+	// Parse the query parameters
+	op := r.URL.Query().Get("op")
+	num1Str := r.URL.Query().Get("num1")
+	num2Str := r.URL.Query().Get("num2")
+
+	// Convert string parameters to integers
+	num1, err1 := strconv.Atoi(num1Str)
+	num2, err2 := strconv.Atoi(num2Str)
+
+	// Check for conversion errors or missing parameters
+	if err1 != nil || err2 != nil || op == "" {
+		http.ServeFile(w, r, "error.html")
+		return
+	}
+
+	// Perform the calculation based on the operation
+	var data PageData
+	tmpl := template.Must(template.ParseFiles("index.html"))
+	switch op {
+	case "add":
+		data = PageData{Expression: fmt.Sprintf("%d + %d", num1, num2), Result: fmt.Sprintf("%d", num1+num2)}
+	case "sub":
+		data = PageData{Expression: fmt.Sprintf("%d - %d", num1, num2), Result: fmt.Sprintf("%d", num1-num2)}
+	case "mul":
+		data = PageData{Expression: fmt.Sprintf("%d * %d", num1, num2), Result: fmt.Sprintf("%d", num1*num2)}
+	case "div":
+		if num2 == 0 {
+			http.ServeFile(w, r, "error.html")
+			return
+		}
+		data = PageData{Expression: fmt.Sprintf("%d / %d", num1, num2), Result: fmt.Sprintf("%d", num1/num2)}
+	case "gcd":
+		data = PageData{Expression: fmt.Sprintf("GCD(%d, %d)", num1, num2), Result: fmt.Sprintf("%d", gcd(num1, num2))}
+	case "lcm":
+		data = PageData{Expression: fmt.Sprintf("LCM(%d, %d)", num1, num2), Result: fmt.Sprintf("%d", lcm(num1, num2))}
+	default:
+		http.ServeFile(w, r, "error.html")
+		return
+	}
+
+	tmpl.Execute(w, data)
+}
+
+func gcd(a, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
+}
+
+func lcm(a, b int) int {
+	return (a * b) / gcd(a, b)
+}
+
+func main() {
+	http.HandleFunc("/", Calculator)
+	log.Fatal(http.ListenAndServe(":8084", nil))
+}
